b2b71f3555df5af0659da504a48f12b92d2ab16f
Brandon Risberg <brisberg@twilight-horizon.com>
2017-06-09 20:59:08 -0700

    Added Exntended sample character template for DS

---
+++ mod/modinfo.lua
@@ -0,0 +1,22 @@
+-- This information tells other players more about the mod
+name = "Whitney"
+description = "A character template."
+author = "Brandon Risberg"
+version = "1.0.4" -- This is the version of the template. Change it to your own number.
+
+-- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
+forumthread = "/files/file/950-extended-sample-character/"
+
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = 6
+
+dont_starve_compatible = true
+
+reign_of_giants_compatible = true
+
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+--configuration_options = {}
---
+++ mod/scripts/prefabs/esctemplate.lua
@@ -0,0 +1,65 @@
+
+local MakePlayerCharacter = require "prefabs/player_common"
+
+
+local assets = {
+
+        Asset( "ANIM", "anim/player_basic.zip" ),
+        Asset( "ANIM", "anim/player_idles_shiver.zip" ),
+        Asset( "ANIM", "anim/player_actions.zip" ),
+        Asset( "ANIM", "anim/player_actions_axe.zip" ),
+        Asset( "ANIM", "anim/player_actions_pickaxe.zip" ),
+        Asset( "ANIM", "anim/player_actions_shovel.zip" ),
+        Asset( "ANIM", "anim/player_actions_blowdart.zip" ),
+        Asset( "ANIM", "anim/player_actions_eat.zip" ),
+        Asset( "ANIM", "anim/player_actions_item.zip" ),
+        Asset( "ANIM", "anim/player_actions_uniqueitem.zip" ),
+        Asset( "ANIM", "anim/player_actions_bugnet.zip" ),
+        Asset( "ANIM", "anim/player_actions_fishing.zip" ),
+        Asset( "ANIM", "anim/player_actions_boomerang.zip" ),
+        Asset( "ANIM", "anim/player_bush_hat.zip" ),
+        Asset( "ANIM", "anim/player_attacks.zip" ),
+        Asset( "ANIM", "anim/player_idles.zip" ),
+        Asset( "ANIM", "anim/player_rebirth.zip" ),
+        Asset( "ANIM", "anim/player_jump.zip" ),
+        Asset( "ANIM", "anim/player_amulet_resurrect.zip" ),
+        Asset( "ANIM", "anim/player_teleport.zip" ),
+        Asset( "ANIM", "anim/wilson_fx.zip" ),
+        Asset( "ANIM", "anim/player_one_man_band.zip" ),
+        Asset( "ANIM", "anim/shadow_hands.zip" ),
+        Asset( "SOUND", "sound/sfx.fsb" ),
+        Asset( "SOUND", "sound/wilson.fsb" ),
+        Asset( "ANIM", "anim/beard.zip" ),
+
+        Asset( "ANIM", "anim/esctemplate.zip" ),
+}
+local prefabs = {}
+local start_inv = {
+	-- Custom starting items
+}
+
+local fn = function(inst)
+	
+	-- choose which sounds this character will play
+	inst.soundsname = "willow"
+
+	-- Minimap icon
+	inst.MiniMapEntity:SetIcon( "esctemplate.tex" )
+	
+	-- Stats	
+	inst.components.health:SetMaxHealth(150)
+	inst.components.hunger:SetMax(150)
+	inst.components.sanity:SetMax(200)
+	
+	-- Damage multiplier (optional)
+    inst.components.combat.damagemultiplier = 1
+	
+	-- Hunger rate (optional)
+	inst.components.hunger.hungerrate = 1 * TUNING.WILSON_HUNGER_RATE
+	
+	-- Movement speed (optional)
+	inst.components.locomotor.walkspeed = 4
+	inst.components.locomotor.runspeed = 6
+end
+
+return MakePlayerCharacter("esctemplate", prefabs, assets, fn, start_inv)
